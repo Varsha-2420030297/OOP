daee0449349fc9ca152c58cbe8464122144ce817
Varsha-2420030297 <2420030297@klh.edu.in>
2025-07-19 11:35:34 +0530

    Add files via upload

---
+++ FIRSTBASICPROGRAMS/src/mypackage/evenodd.java
@@ -0,0 +1,20 @@
+package mypackage;
+import java.util.Scanner;
+
+public class evenodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = scanner.nextInt();
+		if(number%2 == 0) {
+			System.out.println("The number is even");
+		}else {
+			System.out.println("The number is odd");
+		}
+		scanner.close();
+
+	}
+
+}
---
+++ FIRSTBASICPROGRAMS/src/mypackage/first.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	}
+
+}
